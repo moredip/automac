0b8dcc135431790ab5faace3e9f9e41e5bdd8159
Pete Hodgson <git@thepete.net>
2017-04-18 16:12:05 -0700

    WIP on mac setup scripts

---
+++ brew.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e -u
+
+# based on:
+#  - https://github.com/donnemartin/dev-setup/blob/30610914b81c7e638b86e740f62b0b2b303cf09f/brew.sh
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+
+
+brew install \
+  macvim \
+	git \
+	ack \
+	tree \
+	bash-completion
+
+
+brew cask install \
+  google-chrome \
+  skype \
+  dropbox \
+  slack \
+  evernote \
+  1password
---
+++ dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+set -e -u
+
+
+if ! [ -d "~/dotfiles" ]; then
+  cd ~
+  git clone https://github.com/moredip/dotfiles.git
+fi
+
+cd ~/dotfiles
+git submodule update --init --recursive
+./install.rb
