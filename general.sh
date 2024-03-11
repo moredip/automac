ad7fc41e2f49b63d4f18d5f50c28638db0ef16ed
Pete Hodgson <git@thepete.net>
2024-03-10 19:21:10 -0700

    hold off on nvm for now

--- general.sh
+++ general.sh
@@ -3,4 +3,4 @@ set -e -u
 
 [ -d ~/.ssh ] || ssh-keygen
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
