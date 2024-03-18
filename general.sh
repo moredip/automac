13a840dcac111f5312418ac24550d5a2dcf0cab9
Pete Hodgson <git@thepete.net>
2024-03-18 08:16:46 -0700

    install oh-my-zsh

--- general.sh
+++ general.sh
@@ -3,4 +3,8 @@ set -e -u
 
 [ -d ~/.ssh ] || ssh-keygen
 
+# install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# install Oh My Zsh (also needs .zshrc to be installed via dotfiles repo)
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
